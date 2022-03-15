883b1cd2ccb6ffe504e699c70f61a0306428e517
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-16 10:48:07 +1100

    cleaned the AlphabetIndex.js

--- src/components/AlphabetIndex.js
+++ src/components/AlphabetIndex.js
@@ -1,12 +1,5 @@
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import WordsShow from './WordsShow';
-
-import axios from 'axios';
 import './css/AlphabetIndex.css'
-
-
-// const BASE_URL = 'http://localhost:3000/alphabets'
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -15,41 +8,11 @@ function AlphabetIndex() {
 
   const navigate = useNavigate();
 
-  // const [allAbc, setAllAbc] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-
-  //   const fetchData = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const res = await axios.get(BASE_URL);
-  //       console.log('Response', res.data);
-  //       setAllAbc(res.data);
-  //     } catch (err) {
-  //       console.log('Error in search AJAX', err);
-  //       setError(err);
-  //     }
-  //     setLoading(false)
-  //   }
-
-  //   fetchData();
-
-
-  // }, []); //useEffect
-
-  // handleClick = (ev) => {
-  //   ev.preventDefault();
-  //   console.log('check if clicked');
-  // };
-
-
   return (
     <div>
 
       <div className='abcBox'>
-      
+
         {letters.map(letter => (
           <div key={letter} className="alphabetsIndex">
 
