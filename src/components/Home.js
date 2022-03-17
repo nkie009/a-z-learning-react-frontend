cf34047d6d250f1d13f8568f9c2474f0521cbd01
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-18 10:55:16 +1100

    removed about us in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,17 +19,17 @@ export default function Home() {
 
         <div className="azButton" onClick={() => navigate('/alphabets')}>A-Z</div>
 
-        <div>
+        <div className="imgButton">
           <img className="plusButton" src={`./images/plus.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
           <img className="minusButton" src={`./images/minus.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
           <img className="timesButton" src={`./images/times.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
           <img className="divideButton" src={`./images/divide.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
         </div>
 
-        <div className="aboutUs">
+        {/* <div className="aboutUs">
           <h2>About</h2>
           <p>asdf;kjasdf;klas;dlfkj;lsadjf;asd;flkjas;df;asldfj;sldkf</p>
-        </div>
+        </div> */}
       </div>
     </div>
   )
