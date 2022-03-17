bc25cb0d55da6f360a8e093efa2f60d5c6991dca
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-18 10:14:21 +1100

    fixed home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,20 +14,23 @@ export default function Home() {
       <div className="bannerImg">
         <img className="banner1" src={`./images/ed.jpg`} alt="edBanner" />
       </div>
-      
+
       <div className="tableButtons">
 
-        <div className="azButton" onClick={ () => navigate('/alphabets')}>A-Z</div>
+        <div className="azButton" onClick={() => navigate('/alphabets')}>A-Z</div>
 
-        <img className="plusButton" src={`./images/plus.svg`} alt="plus" onClick={ () => navigate('/maths-tables')} />
-        <img className="minusButton" src={`./images/minus.svg`} alt="plus" onClick={ () => navigate('/maths-tables')} />
-        <img className="timesButton" src={`./images/times.svg`} alt="plus" onClick={ () => navigate('/maths-tables')} />
-        <img className="divideButton" src={`./images/divide.svg`} alt="plus" onClick={ () => navigate('/maths-tables')} />
-      </div>
-      <div className="aboutUs">
+        <div>
+          <img className="plusButton" src={`./images/plus.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
+          <img className="minusButton" src={`./images/minus.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
+          <img className="timesButton" src={`./images/times.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
+          <img className="divideButton" src={`./images/divide.svg`} alt="plus" onClick={() => navigate('/maths-tables')} />
+        </div>
+
+        <div className="aboutUs">
           <h2>About</h2>
           <p>asdf;kjasdf;klas;dlfkj;lsadjf;asd;flkjas;df;asldfj;sldkf</p>
         </div>
+      </div>
     </div>
   )
 }
