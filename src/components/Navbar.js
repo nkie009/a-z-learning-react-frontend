7d41b65eb0363c28bda3527d700b7cb91f23d429
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-18 16:37:22 +1100

    fixing NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,18 +14,12 @@ export default function Navbar() {
 
       <img className='logo' src={`./images/azlearning.svg`} alt="azlogo" onClick={ () => navigate("/") }/>
 
-      <a className="azNav" href="/">Home</a>
+      <a className="azHome" href="/">Home</a>
       
-      <a className="azTab" href="/alphabets">A-Z</a>
-      
-      
+      <img className="azTab" src={`./images/abcNavbar.svg`} alt="times" onClick={ () => navigate('/alphabets')} />
 
       <img className="timesTab" src={`./images/times.svg`} alt="times" onClick={ () => navigate('/maths-tables')} />
 
-
-      {/* <img className="minusTab" src={`./images/minus.svg`} alt="plus" onClick={ () => navigate('/maths-table')} />
-      <img className="timesTab" src={`./images/times.svg`} alt="plus" onClick={ () => navigate('/maths-table')} />
-      <img className="divideTab" src={`./images/divide.svg`} alt="plus" onClick={ () => navigate('/maths-table')} /> */}
     </div>
 
     </div>
