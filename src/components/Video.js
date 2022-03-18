cc0aae97761111ce2f5d101a535d1fe1c58e4d0d
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-18 23:32:04 +1100

    added css to video page

--- src/components/Video.js
+++ src/components/Video.js
@@ -5,41 +5,50 @@ export default function Video() {
   return (
     <div className="kidsTube">
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/yJtNLPhDtJw"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
+      <div className="v1">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/yJtNLPhDtJw"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/NG809Re-SIs"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
+      <div className="v2">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/NG809Re-SIs"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/-gdcgnSrUvU"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
 
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/HGeuA4iJ8vI"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-      </iframe>
+      <div className="v3">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/-gdcgnSrUvU"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
+
+      <div className="v4">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/HGeuA4iJ8vI"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
 
     </div>
   )
