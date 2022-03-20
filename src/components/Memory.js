125739328be08b7828b8947ecc619d3cac1e427f
Frankie Tsoi <frankie.tsoi@hotmail.com>
2022-03-20 13:27:23 +1100

    added memory game

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -3,12 +3,12 @@ import './css/Memory.css'
 import SingleCard from "./SingleCard";
 
 const cardImages = [
-  { "src": "/images/apple.svg" },
-  { "src": "/images/banana.svg" },
-  { "src": "/images/cake.svg" },
-  { "src": "/images/duck.svg" },
-  { "src": "/images/egg.svg" },
-  { "src": "/images/frog.svg" }
+  { "src": "/images/apple.svg", matched: false },
+  { "src": "/images/banana.svg", matched: false },
+  { "src": "/images/cake.svg", matched: false },
+  { "src": "/images/duck.svg", matched: false },
+  { "src": "/images/egg.svg", matched: false },
+  { "src": "/images/frog.svg", matched: false } //added matched property to set it as false
 ]
 
 
@@ -18,23 +18,69 @@ export default function Memory() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null) //first click will updates the choice one
   const [choiceTwo, setChoiceTwo] = useState(null) //second click will updates the choice two
-
+  const [disabled, setDisabled] = useState(false)
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
-      //duplicate the above array 'carImages' into line 18
+      //duplicate the above array 'carImages' into here
       .sort(() => Math.random() - 0.5) //each pair item on this array. if the order returns less than 0 the order of item stay the same. if the its greater than it will be swap. Math.random give you a random from 0 - 1. 
       .map((card) => ({ ...card, id: Math.random() }))
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards)//to update the state
     setTurns(0)// every time click the new game button will reset the game
   }
-  console.log('check here for shuffleCards', cards, turns)
-  
+  // console.log('check here for shuffleCards', cards, turns)
+
   //handle a choice
   const handleChoice = (card) => {
     // console.log('checked card',card);
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }// if choiceOne is null which means we dont have a selection for ChoiceOne, if choiceOne has a value which is selected then setChoiceTwo will run and update choice two
+  }// if choiceOne is null which means we dont have a selection for ChoiceOne, if choiceOne has a value which is selected then setChoiceTwo will run and update choice two state
+
+  //compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      // if choiceOne src and choiceTwo src are the same then we have a match, then we update the setCards state
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            //if the card src matches the selected choice src, return card array to matched: true
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              // if not match then return card to unchanged
+              return card
+            }
+          })
+        })
+        // console.log('those cards are match');
+        resetTurn()//to reset turn function
+
+      } else {
+        console.log('those cards do not match');
+        setTimeout(() => resetTurn(), 1100)
+      }
+    }
+  }, [choiceOne, choiceTwo])// passing dependencies of the array as an augment choiceOne and choiceTwo.
+  //useEffect will fire when the component first mount and then fired again when a dependency changes.
+
+  console.log('check cards state', cards);
+
+  //reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  //the below is to start the game automically 
+  useEffect(() =>{
+    shuffleCards()
+  }, [])
 
   return (
     <div className="game">
@@ -43,16 +89,22 @@ export default function Memory() {
       <button className="newGameButton" onClick={shuffleCards}>New Game</button>
 
 
-    <div className="cardGrid">
+      <div className="cardGrid">
 
-      {cards.map(card =>(
-        <SingleCard key={card.id} card={card} handleChoice={handleChoice}/> 
-        /* here is to pass down props 'card' to Single Card componenet */
-      ))}
-    </div>
+        {cards.map(card => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+          /* here is to pass down props 'card' to Single Card component */
+        ))}
+      </div>
 
 
-
+      <p>Turns: {turns}</p>
 
     </div>
   )
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './css/SingleCard.css'
 
-//the below has pass down the card and handleChoice props from Memory componenet
-export default function SingleCard({card, handleChoice}) {
+//the below has pass down the card, handleChoice and flipped props from Memory component
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
 
   const handleClick = () => {
-    handleChoice(card)
+    if(disabled === false){
+      handleChoice(card)
+    }
   } // here is to update the state, also pass card to the prop handleChoice
 
   return (
     <div  className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="frontCard" />
         <img className="back" src="/images/azlearning.svg" alt="backCard" onClick={handleClick} />
       </div>
